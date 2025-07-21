c94ed0ebdbfc6342c6fe7e59063326ad078c6d48
patrickcwf <patrick.canniff@webflow.com>
2025-07-21 11:37:03 -0700

    Fix next.config for OpenNext build compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // You can set custom config here, but don’t manually set `basePath` or `assetPrefix`
+  reactStrictMode: true,
+};
 
 export default nextConfig;
